9814ab24213962fec90e49adfaffc69a12a140ba
PJ Fiers <pjfiers@gmail.com>
2011-12-21 16:02:16 +0100

    removed commented code

--- init.php
+++ init.php
@@ -35,29 +35,3 @@ $registry->router->load();
 $mongo = new Mongo();
 $db = $mongo->runwiththetorch;
 $registry->db = $db;
-
-/**
-$id = 'testBlogId1';
-/*$ent = new Rwtt_Entity_BlogPost();
-$ent->setId($id);
-$ent->save();
-
-
-$ent = new Rwtt_Entity_BlogPost();
-if($ent->loadById($id)) {
-    $ent->title = 'testing title 123';
-    $ent->author = 'PJ';
-    $ent->content = 'this is some <br/>' . PHP_EOL
-                  . '<strong>HTML</strong> content';
-    $ent->save();
-} else {
-    echo 'id ' . $id . 'not found' . PHP_EOL;
-}
-
-var_dump($ent);
-echo PHP_EOL . PHP_EOL;
-$cursor = $db->blogposts->find();
-foreach ($cursor as $doc) {
-    var_dump($doc);
-}
-//$db->entities->remove(array());*/
