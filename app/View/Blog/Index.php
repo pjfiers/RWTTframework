f737513fc80f950d60388a6b80d3641cdab430f0
PJ <PJ@192.168.1.3>
2012-07-13 15:24:11 +0200

    added date to view

--- app/View/Blog/Index.php
+++ app/View/Blog/Index.php
@@ -15,10 +15,14 @@ $title->appendChild($titleAppend);
 $content = $this->_registry->content;
 $h1 = $htmlDom->createElement('h1', $this->title);
 $content->appendChild($h1);
-
 foreach ($this->items as $item) {
     $h2 = $htmlDom->createElement('h2', $item['title']);
     $content->appendChild($h2);
+    $dateString = 'created on: ' . $item['_creationDate'];
+    $date = $content->appendChild(
+        $htmlDom->createElement('p', $dateString)
+    );
+    $date->setAttribute('class', 'date');
     $fragment = $htmlDom->createDocumentFragment();    
     $fragment->appendXML($item['content']);
     $div = $htmlDom->createElement('div');
